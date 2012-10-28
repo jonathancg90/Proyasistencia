42bae27a1f8acd5821bafc8cb10a24dd07235038
platano <platano@ubuntu.(none)>
2012-10-28 09:32:57 -0700

    Creacion de proyecto

---
+++ Sis_Asistencia/src/sis_asistencia/Sis_Asistencia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sis_asistencia;
+
+/**
+ *
+ * @author platano
+ */
+public class Sis_Asistencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
