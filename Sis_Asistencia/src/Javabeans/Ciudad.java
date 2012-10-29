a41b8077b3b29aa37fbd6b743d7754876c3a24d5
JohnProg <platano@ubuntu.(none)>
2012-10-28 21:53:54 -0500

    Creacion de Clase Ciudad

---
+++ Sis_Asistencia/src/Javabeans/Ciudad.java
@@ -0,0 +1,49 @@
+
+package Javabeans;
+
+
+public class Ciudad {
+    private int idciu;
+    private String name;
+
+    /* Constructor final
+     */
+    public Ciudad(int idciu, String name) {
+        this.idciu = idciu;
+        this.name = name;
+    }
+    
+    /* Constructor por omisión
+     */
+    public Ciudad() {
+    }
+
+    /**
+     * @return the idciu
+     */
+    public int getIdciu() {
+        return idciu;
+    }
+
+    /**
+     * @param idciu the idciu to set
+     */
+    public void setIdciu(int idciu) {
+        this.idciu = idciu;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
