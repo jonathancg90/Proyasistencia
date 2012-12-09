bfbc08bc62b41d89937c84be68a0925860f954df
JSC <jesus_jvl1@hotmail.com>
2012-12-08 21:57:31 -0500

    creacion del formulario tipo horario

---
+++ Sis_Asistencia/src/Javabeans/Tipohorario.java
@@ -0,0 +1,34 @@
+
+package Javabeans;
+
+
+public class Tipohorario {
+    private int idtiphor;
+    private String name;
+
+    public Tipohorario() {
+    }
+
+    public Tipohorario(int idtiphor, String name) {
+        this.idtiphor = idtiphor;
+        this.name = name;
+    }
+
+    public int getIdtiphor() {
+        return idtiphor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIdtiphor(int idtiphor) {
+        this.idtiphor = idtiphor;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
