755cee957e19ab52921342b129cbcd090afc6415
Jonathancg <john.cfmr.2009@gmail.com>
2012-12-25 23:31:21 -0500

    Creacion de Extra_descuentoDAO

---
+++ Sis_Asistencia/src/Dao/Extra_descuentoDAO.java
@@ -0,0 +1,194 @@
+
+package Dao;
+
+import Javabeans.Extra_descuento;
+import Utilitarios.Helpers;
+import Utilitarios.ConexionBd;
+import Utilitarios.Query;
+import Utilitarios.Validators;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+public class Extra_descuentoDAO extends ConexionBd{
+    
+    private Query qs;
+    private Extra_descuento objdes;
+    private Helpers hp;
+    private Validators objVal;
+    private String filter[][];
+    private String campos[];
+    private int witdhcolum[];
+    private String _table;
+    private String _error;
+    
+    PreparedStatement  pt = null;
+    
+     public Extra_descuentoDAO(){
+        _table = "Extra_descuento";
+        _error = "Dao_Extra_descuentoDAO_";
+        filter = new String[0][0];
+        campos = new String[2];
+        campos[0]="id";
+        campos[1]="tipo";
+        witdhcolum = new int[1];
+        witdhcolum[0]=50;
+    }
+      //==========================================================================        
+    public void getTableAll(JTable tblDatos){
+        try{
+            DefaultTableModel datos;
+            qs= new Query();
+            hp = new Helpers();
+            if (filter.length <= 0){
+                filter = new String[0][0];
+            }
+            String Table = this._table;
+            datos = qs.getAll(this.campos,Table,filter);
+            tblDatos.setModel(datos);
+            hp.setWidthJtable(tblDatos,witdhcolum);
+        }
+        catch(Exception e){
+            System.out.println(_error + "getTableAll: "+e);
+        }
+    
+    }
+    /*
+     * Registro de areas
+     */
+    public int save(String name, boolean state,int tipo,int iddet_hor){
+       int i=0;
+        try{
+            Date date = new Date(0000-00-00);
+            //Preparando
+            getConexion();
+            hp = new Helpers();
+            qs = new Query();
+            String Table = this._table;
+            String now = hp.getDateNow();
+            
+            objdes = new Extra_descuento(i, tipo, name, tipo, i);
+            //Iniciando consulta y asignando valores
+            pt = qs.sqlRegister(Table);
+            pt.setInt(1,objdes.getTipo());
+            pt.setDate(2,date.valueOf(objdes.getDate()));
+            pt.setFloat(3,objdes.getMonto());
+            pt.setInt(4,objdes.getIdemp());
+            //Ejecucion y cierre
+            i= pt.executeUpdate();
+            pt.close();
+            closeConexion();
+            return i;
+        }
+        catch(Exception e){
+            System.out.println(_error + "save: "+e);
+            return i;
+        }
+    }
+    /*
+     * Actualizacion de areas
+     */
+    public int update(int id,String name,int tipo,boolean state,int iddet_hor){
+       int i=0;
+        try{
+            Date date = new Date(0000-00-00);
+            //Preparando
+            getConexion();
+            hp = new Helpers();
+            qs= new Query();
+            String Table = this._table;
+            String now = hp.getDateNow();
+            
+            objdes = new Extra_descuento(i, tipo, name, tipo, i);
+            //Iniciando consulta y asignando valores
+            pt = qs.sqlRegister(Table);
+            pt.setInt(1,objdes.getTipo());
+            pt.setDate(2,date.valueOf(objdes.getDate()));
+            pt.setFloat(3,objdes.getMonto());
+            pt.setInt(4,objdes.getIdemp());
+            //Ejecucion y cierre
+            i= pt.executeUpdate();
+            pt.close();
+            closeConexion();
+            return i;
+        }
+        catch(Exception e){
+            System.out.println(_error + "update: "+e);
+            return i;
+        }
+    }
+     /*
+     * Eliminar 
+     */
+    public int delete(int id){
+       int i=0;
+        try{
+            //Preparando
+            getConexion();
+            objdes = new Extra_descuento();
+            hp = new Helpers();
+            qs= new Query();
+            String Table = this._table;
+            
+            objdes.setId(id);
+            pt = qs.sqlDelete(Table);
+            pt.setInt(1,objdes.getId());
+            i= pt.executeUpdate();
+            pt.close();
+            closeConexion();
+            return i;
+        }
+        catch(Exception e){
+            System.out.println(_error + "delete: "+e);
+            return i;
+        }
+    }
+    /*
+     * Por defecto busca a travez de un like '%%'
+     * int_ = Busqueda de entero
+     * equ_ = busca exactamente la plabra
+     */
+   /*
+     * Filtros de busqueda
+     */
+    public int find(String state,JTable tblDatos) {
+        int i = 0;
+        try {
+            if(!"".equals(state)){
+                filter = new String[1][2];
+                filter[0][0] = "equ_estado";
+                filter[0][1] = state; 
+            }
+            getTableAll(tblDatos);
+        }
+        catch(Exception e){
+            System.out.println(_error + "find : "+e);
+        }
+        return i;
+    }
+
+    /*
+     * Cargar valores de busqueda al modelo 
+     */
+    public Extra_descuento getValues(int idusu){
+       objdes =  new Extra_descuento();
+       objVal = new Validators();
+        try{
+            qs= new Query();
+            //Preparando
+            String campos[] = new String[6];
+            campos = qs.getRecords(_table,idusu);
+            objdes.setTipo(Integer.parseInt(campos[2]));
+            objdes.setDate(campos[3]);
+            objdes.setMonto(Float.valueOf(campos[4]));
+            objdes.setIdemp(Integer.parseInt(campos[5]));
+            
+            return objdes;
+        }
+        catch(Exception e){
+            System.out.println(_error + "getValues: "+e);
+            return objdes;
+        }
+    }
+}
+
