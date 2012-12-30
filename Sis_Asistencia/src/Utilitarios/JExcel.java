39c858987dd70cf6c091d084b6a9d6b65e0b37bb
Jonathancg <Jonathancg90@gmail.com>
2012-12-30 15:38:49 -0800

    Metodo para exportar a excel

---
+++ Sis_Asistencia/src/Utilitarios/JExcel.java
@@ -0,0 +1,80 @@
+package Utilitarios;
+import java.io.File; 
+import java.io.IOException;
+import java.util.Calendar;
+
+import java.util.Date; 
+import jxl.*; 
+import jxl.write.*; 
+import jxl.write.Number;
+
+public class JExcel {
+ 
+    private WritableWorkbook workbook;
+    private WritableSheet sheet;
+    private WritableFont Arialfont, Timesfont;
+    private WritableCellFormat Arialcellfont, Timescellfont,
+            IntegerFormat,FloatFormat,fivedpsFormat,dateFormat;
+    
+     public static void main(String[] args) throws IOException, WriteException {
+         JExcel xls = new JExcel();
+         xls.CreateXls("empleado.xls");
+         xls.ExcelTest();
+    }
+    public JExcel() throws WriteException{
+       //Tipo de letra
+            //Arial
+            Arialfont = new WritableFont(WritableFont.ARIAL, 10);
+            Arialfont.setColour(jxl.format.Colour.BLUE);
+            Arialcellfont = new WritableCellFormat (Arialfont); 
+            //Times Roman(16,Bold)
+            Timesfont = new WritableFont(WritableFont.TIMES, 16, WritableFont.BOLD, true); 
+            Timescellfont = new WritableCellFormat (Timesfont); 
+        //Formato de celda
+            //Formato para enteros
+            IntegerFormat = new WritableCellFormat (NumberFormats.INTEGER);
+            //Formato para float(dos decimales)
+            FloatFormat = new WritableCellFormat (NumberFormats.FLOAT); 
+            //Formato de numero personalizado
+            NumberFormat fivedps = new NumberFormat("#.#####"); 
+            fivedpsFormat = new WritableCellFormat(fivedps); 
+            //Formato fecha
+            DateFormat customDateFormat = new DateFormat ("dd MMM yyyy hh:mm:ss"); 
+            dateFormat = new WritableCellFormat (customDateFormat); 
+        
+    }
+     
+    public void CreateXls(String file) throws IOException, WriteException {
+        //Creacion del libro
+        workbook = Workbook.createWorkbook(new File(file)); 
+        sheet = workbook.createSheet("test", 0);
+        
+    }
+    public void ExcelTest() throws WriteException, IOException{
+        //Columna,Fila(Comienza de 0)
+        Label label = new Label(0, 1, "Informacion del empleado",Arialcellfont); 
+        sheet.addCell(label); 
+
+        Number number = new Number(3, 4, 3.1459,Timescellfont); 
+        sheet.addCell(number);
+        
+        Number number2 = new Number(0, 4, 3.141519, IntegerFormat); 
+        sheet.addCell(number2); 
+
+        Number number3 = new Number(1, 4, 3.141519, FloatFormat); 
+        sheet.addCell(number3);
+        
+        Number number4 = new Number(2, 4, 2.141519, fivedpsFormat); 
+        sheet.addCell(number4);
+        
+        Date now = Calendar.getInstance().getTime(); 
+        DateTime dateCell = new DateTime(0, 6, now, dateFormat); 
+        sheet.addCell(dateCell); 
+        
+        
+        workbook.write(); 
+        workbook.close();
+        
+        
+    }
+}
